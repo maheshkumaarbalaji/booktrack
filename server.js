932f94955292f47209b4eee9b90ad6a8f3e8b946
maheshkumaar <maheshkumaar2106@gmail.com>
2016-09-25 02:10:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,6 +47,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 var comments=[];
 app.get('/submit-comment',function(req,res){
 var comment=req.query.comment;
