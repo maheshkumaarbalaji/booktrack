c54ec169a0db8b2e8984e02713ffebe41145684e
maheshkumaar <maheshkumaar2106@gmail.com>
2016-09-25 02:16:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ ul.innerHTML=list;
 };
 var Input=document.getElementById('comm');
 var comment=Input.value;
-request.open('GET','http://127.0.0.1:8081/submit-comment?comment='+comment,true);
+request.open('GET','http://maheshkumaar.imad.hasura-app.io/submit-comment?comment='+comment,true);
 request.send(null);
 };
 
