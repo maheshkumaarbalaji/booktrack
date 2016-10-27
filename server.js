cd03f02c1453d98a6d974808ff7e0005283a6501
maheshkumaar <maheshkumaar2106@gmail.com>
2016-10-27 10:04:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ res.send(JSON.stringify(comments));
 
 
 app.get('/articles/:articleName', function (req, res) {
-pool.query("SELECT * FROM article WHERE name='" +  req.params.articleName + "'",function(err,result){
+pool.query("SELECT * FROM article WHERE name=$1" ,[req.params.articleName] ,function(err,result){
     if(err){
         res.status(500).send(err.toString());
     }
