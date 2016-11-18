56426d44ab4b1c30eec5e219511c1719fd24f309
maheshkumaar <maheshkumaar2106@gmail.com>
2016-11-18 19:53:34 +0530

    [imad-console] Updates ui/User.js

--- ui/User.js
+++ ui/User.js
@@ -8,6 +8,28 @@ request.onreadystatechange=function(){
         if(request.status===200)
         {
             var result=JSON.parse(request.responseText);
+            var htmlcontent=`
+            <table>
+            <tr>
+            <th>BookID</th>
+            <th>Title</th>
+            <th>Genre</th>
+            </tr>
+            `;
+            for(var i=0;i<result.length;i++)
+            {
+                htmlContent+=`<tr>
+                <td>${result[0].BookId}</td>
+                <td><a href="/browse-books/${result[0].Title}">${result[0].Title}</a></td>
+                <td>${result[0].Genre}</td>
+                </tr>
+                `;
+            }
+            document.getElementById("context_area").innerHTML=htmlcontent;
+        }
+        else
+        {
+            alert("Some error occurred! Try again later.")
         }
     }
 };
