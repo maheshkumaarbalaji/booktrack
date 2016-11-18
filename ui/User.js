1af75494ae5585ffb297bb52fe7f0582d69357db
maheshkumaar <maheshkumaar2106@gmail.com>
2016-11-18 19:09:27 +0530

    Create User.js

---
+++ ui/User.js
@@ -0,0 +1,7 @@
+var button1=document.getElementById("button1");
+button1.onclick=function(){
+document.getElementById("context_title").innerHTML='Add/Manage Books';
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+};
+};
