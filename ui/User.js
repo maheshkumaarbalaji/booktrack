65eeefe78c7bfeaddc5c68a03a793a6ad48301de
maheshkumaar <maheshkumaar2106@gmail.com>
2016-11-18 19:18:17 +0530

    [imad-console] Updates ui/User.js

--- ui/User.js
+++ ui/User.js
@@ -3,5 +3,14 @@ button1.onclick=function(){
 document.getElementById("context_title").innerHTML='Add/Manage Books';
 var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {
+            var result=JSON.parse(request.responseText);
+        }
+    }
 };
+request.open("GET","/browse-books",true);
+request.send(null);
 };
