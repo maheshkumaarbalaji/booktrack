264dd321764df9ee7371e2c1c995e06abb73f6d8
maheshkumaar <maheshkumaar2106@gmail.com>
2016-11-25 19:12:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,23 +20,7 @@ function loadLogin()
     request.send(null);
 }
 
-function checkRegister(username)
-{
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function()
-    {
-        if(request.readyState===XMLHttpRequest.DONE)
-        {
-            if(request.status===200)
-                return 1;
-            else
-                return 0;
-        }
-    };
-    request.open("POST","/check-register",true);
-    request.setRequestHeader("Content-Type","application/json");
-    request.send(JSON.stringify({"username":username}));
-}
+
 
 
 function loadLoginForm()
@@ -84,9 +68,6 @@ function loadLoginForm()
     {
         var username=document.getElementById("username").value;
         var password=document.getElementById("password").value;
-        var check=checkRegister(username);
-        if(check===1)
-        {
             var request=new XMLHttpRequest();
             request.onreadystatechange=function()
             {
@@ -109,11 +90,6 @@ function loadLoginForm()
             request.setRequestHeader("Content-Type","application/json");
             request.send(JSON.stringify({"username":username,"password":password}));
             submit.value="Creating user..";
-        }
-        else
-        {
-            alert('Username already exists!');
-        }
 
     };
 }
